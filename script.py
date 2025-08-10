20e08928ecd1eeff24a9d584469e50080e8788ad
Stas <stasnovalenco3@gmail.com>
2025-08-10 19:59:31 +0300

    first commit

---
+++ script.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from mistralai import Mistral
+
+load_dotenv()
+api_key= os.environ["API_KEY"]
+model= "mistral-small-latest"
+
+client= Mistral(api_key=api_key)
+
+
+def get_response(content):
+    chat_response= client.chat.complete(
+        model= model,
+        messages= [
+            {
+                "role": "system",
+                "content": "My chatbot assistent for code"
+            },
+            {
+                "role": "user",
+                "content": content
+            }
+        ]
+    )
+    return chat_response.choices[0].message.content
+
+response= get_response("Hi! Where are you from?")
+print(response)
+# print("----")
+
+# responses= []
+# for i in range(10):
+#     if i == 0:
+#         responses.append(get_response(response))
+#     else:
+#         responses.append(get_response(responses[i-1]))
+
+# for message in responses:
+#     print(message)
+    
